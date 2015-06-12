af82825ab17b165a5a4f7457e97ff174dd3096d2
Jamie <jmeshen@gmail.com>
2015-06-12 11:28:19 -0400

    actually working search

--- server/app/routes/search/index.js
+++ server/app/routes/search/index.js
@@ -5,16 +5,17 @@ var _ = require('lodash');
 var mongoose = require('mongoose');
 var Listings = mongoose.model('Listing');
 
-router.post('/', function(req, res, next) {
-    var wordRegExp = new RegExp(req.body.searchStr, "i");
+router.get('/', function(req, res, next) {
+    var wordRegExp = new RegExp(req.param.searchStr, "i");
     var categoryArray;
-    if (!req.body.categoryId) {
+    if (!req.query.categoryId) {
       console.log('no categorieszzzzzzzzzzZZZZZZZZZZZZ')
+      // console.log(req.query);
       Listings.find({ title: wordRegExp }).exec().then(function(listings) {
           res.send(listings);
       }).then(null, next);
     } else {
-      categoryArray = req.body.categoryId.split(',');
+      categoryArray = req.query.categoryId.split(',');
       Listings.find({ title: wordRegExp, category: {$in: categoryArray }})
         .exec()
         .then(function(listings) {
