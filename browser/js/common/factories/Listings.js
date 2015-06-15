dca793ec89c80cfa1cbe6b6e993d3fbfc1481247
Michael Barzizza <mbarzizza1@gmail.com>
2015-06-15 12:35:25 -0400

    local auth almost working

--- browser/js/common/factories/Listings.js
+++ browser/js/common/factories/Listings.js
@@ -7,8 +7,15 @@ app.factory('Listings',function($http){
 		},
 		createListing:function(formData){
 			// console.log("listings factory form data: ",formData);
-			return $http.post('/api/listings/create',{category:formData.category,title:formData.title,description:formData.description,price:formData.price,quantity:formData.quantity}).then(function(response){
-			});
+			return $http.post('/api/listings/create',{
+				category:formData.category,
+				title:formData.title,
+				description:formData.description,
+				price:formData.price,
+				quantity:formData.quantity
+			}).then(function(response){
+				console.log(response); 
+			})
 		},
 		getOne:function(id){
 			return $http.get('/api/listings/' + id).then(function(response) {
