5755431d384959a0794538b92edbf06da9b053df
Tyler Perkins <threeperkins@gmail.com>
2015-06-15 12:36:30 -0400

    fixed some errors

--- browser/js/signup/create.js
+++ browser/js/signup/create.js
@@ -13,11 +13,12 @@ app.controller('signupController', function ($scope,$window,AuthService, Signup)
 
     $scope.SignupForm;
     $scope.createUser = function() {
-    	console.log("called create user");
-        Signup.createUser($scope.SignupForm).then(response){
-        	console.log("User creation response ",response);
-        };
+        console.log("called create user");
+        Signup.createUser($scope.SignupForm).then(function (response) {
+            console.log("User creation response ", response);
+        })
     }
+
      $scope.googleAuth = function(){
         console.log("Authenticating with Google OAuth2");    
         $window.location.href="/auth/google"; 
