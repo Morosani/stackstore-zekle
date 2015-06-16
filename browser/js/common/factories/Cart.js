f72d693cc7e0303c82a617a80e71fffe68d26231
Jamie <jmeshen@gmail.com>
2015-06-16 15:54:39 -0400

    fixed cart issue and added browserified Stripe dependency

--- browser/js/common/factories/Cart.js
+++ browser/js/common/factories/Cart.js
@@ -1,11 +1,15 @@
 app.factory('Cart',['locker' ,function(locker){
-	// locker.put('cart',[]);
+
+	if (!locker.get('cart')) {
+		locker.put('cart',[]);
+	}
+
 	return {
 		getCart:function(){
-			return locker.get('cart'); 
+			return locker.get('cart');
 		},
 		getTotal:function(){
-			return locker.get('total'); 
+			return locker.get('total');
 		},
 		addToCart:function(listing){
 			locker.put('cart',function(current){
@@ -14,23 +18,23 @@ app.factory('Cart',['locker' ,function(locker){
 			});
 		},
 		remove: function(id){
-			var cart = locker.get('cart'); 
+			var cart = locker.get('cart');
 			cart.forEach(function(listing,index){
 				if(listing['_id'] === id){
 					cart.splice(index,1);
 				}
 			});
 			locker.put('cart',cart);
-			return cart; 
+			return cart;
 		},
 		updateQty:function(id,qty){
-			var cart = locker.get('cart'); 
+			var cart = locker.get('cart');
 			cart.forEach(function(listing){
 				if(listing['_id'] === id){
 					listing.quantity=qty;
 				}
 			});
-			locker.put('cart',cart); 
+			locker.put('cart',cart);
 			return cart;
 		},
 		emptyCart:function(){
